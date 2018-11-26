835655ba6192fdb91ef56e61b7d05b7f1fdc7287
marxin <mliska@suse.cz>
2018-11-26 15:15:13 +0100

    Do a bigger loop, report comparison.

--- test.c
+++ test.c
@@ -6,30 +6,30 @@ extern int foo_no_table (int);
 
 int global = 20;
 
-#define LOOP 30000
+#define LOOP 800000000
 
 int
 main ()
 {
   int i;
   unsigned long long start, end;
-  unsigned long long diff;
+  unsigned long long diff1, diff2;
 
   start = __rdtscp (&i);
   for (i = 0; i < LOOP; i++)
     foo_no_table (i);
   end = __rdtscp (&i);
-  diff = end - start;
+  diff1 = end - start;
 
-  printf ("no jump table: %lld\n", diff);
+  printf ("no jump table: %lld\n", diff1);
 
   start = __rdtscp (&i);
   for (i = 0; i < LOOP; i++)
     foo (i);
   end = __rdtscp (&i);
-  diff = end - start;
+  diff2 = end - start;
 
-  printf ("jump table   : %lld\n", diff);
+  printf ("jump table   : %lld (%.2f%%)\n", diff2, 100.0f * diff2 / diff1);
 
   return 0;
 }
